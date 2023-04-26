cbf600d4a9b050ddd79ae04a0c6521cda2842fd6
Tom Lacor <tomlacor@gmail.com>
2023-04-26 09:40:03 +0200

    controller updated

--- src/Controller/BoardgameController.php
+++ src/Controller/BoardgameController.php
@@ -3,16 +3,23 @@
 namespace App\Controller;
 
 use App\Entity\Boardgame;
+use App\Form\BoardgameType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Doctrine\ORM\EntityManagerInterface;
 
 class BoardgameController extends AbstractController
 {
     private $entityManager;
 
-    #[Route('/boardgames', name: 'boardgames')]
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/boardgames', name: 'app_boardgame')]
 
     public function show(EntityManagerInterface $entityManager): Response
     {
@@ -20,6 +27,25 @@ class BoardgameController extends AbstractController
 
         return $this->render('boardgames/show.html.twig', [
             'boardgames' => $boardgames
+        ]);
+    }
+
+    #[Route('/boardgames/new', name: 'app_boardgame_new')]
+    public function new(Request $request): Response
+    {
+        $boardgame = new Boardgame();
+        $form = $this->createForm(BoardgameType::class, $boardgame);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($boardgame);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('app_boardgame');
+        }
+
+        return $this->render('boardgames/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 
