27c054db442d69a29e691818b315e75505834571
Tom Lacor <tomlacor@gmail.com>
2023-05-08 12:37:05 +0200

    user roles updated + reroutes adapted

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,6 +11,9 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+
+   
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -31,25 +34,9 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    
+        
     }
 
-    #[Route(path: '/promote-tom-to-admin', name: 'promote_tom_to_admin')]
-    public function promoteTomToAdmin(EntityManagerInterface $entityManager): Response
-    {
-        $userRepository = $entityManager->getRepository(User::class);
-        $tom = $userRepository->findOneByEmail('tom@mail.com');
-
-        if (!$tom) {
-            throw $this->createNotFoundException('Tom not found');
-        }
-
-        $tom->setRoles(['ROLE_ADMIN']);
-        $entityManager->flush();
-
-        if ($this->isGranted('ROLE_ADMIN')) {
-            return $this->redirectToRoute('app_boardgame');
-        }
-
-        return $this->redirectToRoute('app_boardgame_user');
-    }
+    
 }
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -2,18 +2,19 @@
 
 namespace App\Security;
 
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
-use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 
 class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 {
@@ -31,26 +32,37 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 
         $request->getSession()->set(Security::LAST_USERNAME, $email);
 
+        $userRoles = ['ROLE_USER'];
+
+    // check if the user is Tom and set their roles to ROLE_ADMIN
+    if ($email === 'tom@mail.com') {
+        $userRoles[] = 'ROLE_ADMIN';
+    }
+
         return new Passport(
             new UserBadge($email),
             new PasswordCredentials($request->request->get('password', '')),
             [
                 new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
-            ]
+            ],
+            new User($email, null, $userRoles)
         );
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
-    {
-        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
-            return new RedirectResponse($targetPath);
-        }
+{
+    if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+        return new RedirectResponse($targetPath);
+    }
 
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+    $user = $token->getUser();
+
+    if ($user instanceof User && in_array('ROLE_ADMIN', $user->getRoles())) {
         return new RedirectResponse($this->urlGenerator->generate('app_boardgame'));
     }
+
+    return new RedirectResponse($this->urlGenerator->generate('app_boardgame_user'));
+}
 
     protected function getLoginUrl(Request $request): string
     {
